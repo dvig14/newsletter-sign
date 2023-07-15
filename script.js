0d0c32a5f98626a21bfd5682c4c542f105f5fdf2
Diksha <97282430+dvig14@users.noreply.github.com>
2023-07-15 22:22:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const form=document.querySelector('form');
+const email=document.querySelector('input');
+const sButton=document.getElementById('success-btn');
+const dButton=document.getElementById('dismiss-btn');
+
+form.addEventListener('submit',validate);
+sButton.addEventListener('click',validate);
+dButton.addEventListener('click',()=>{
+    location.reload();
+});
+
+function validate(e){
+    e.preventDefault();
+    const emailVal=email.value.trim();
+    if(emailVal==''){
+        setError(email,'Email is required')
+    }
+    else if(!isEmail(emailVal)){
+        setError(email,'Valid email is required')
+    }
+    else{
+        success(email);
+    }
+}
+function setError(id,error){
+      const card=id.parentElement;
+      const span=card.querySelector('#txt').lastElementChild;
+      span.innerText=error;
+      span.className='err'; 
+      id.className='err-txt';
+      span.style.display='block';
+}
+function isEmail(email){
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+}
+function success(id){
+    id.className='success';
+    const card=id.parentElement;
+      const span=card.querySelector('#txt').lastElementChild;
+      span.style.display='none';
+    const grandParent=card.parentElement;
+    const garndsParent=grandParent.parentElement;
+    const successCard=document.querySelector('.wrapper-2');
+    setTimeout(()=>{
+         garndsParent.classList.add('hidden');
+         successCard.classList.remove('hidden');
+    },800);
+}
